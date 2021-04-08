c664463d95d6ef540fd4e6b4039f43a65a9b2381
Sanele <sanelesithole001@gmail.com>
2021-04-08 14:57:07 +0200

    adding multiple code

---
+++ src/multiplication/Multiply.java
@@ -0,0 +1,31 @@
+package multiplication;
+
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the  start Number: ");
+		int start = input.nextInt();
+		
+		System.out.println("Enter the end Number: ");
+		int end = input.nextInt();
+		
+		System.out.println("Enter Multiple: ");
+		int multiple = input.nextInt();
+		
+		System.out.println("***Displaying the "+ multiple + " Times table");
+		for(int i = start ; i <=  end;i++)
+		{
+			System.out.println(multiple + "x"+ i + "= "+ multiple*i );
+		}
+		
+		System.out.println("******************************* ");
+		
+		
+	}
+
+}
